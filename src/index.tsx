e9976d7ab874e07714fbad95e7114b8a1196b8c4
Joseph Lyon <joeylyon1@gmail.com>
2024-07-23 19:06:23 -0400

    heic-to-jpeg

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ImageMagick, initializeImageMagick, MagickFormat } from '@imagemagick/magick-wasm';
+
+const Idx = () => {
+    const [files, setFiles] = useState<File[]>([]);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            setFiles(Array.from(event.target.files));
+        }
+    };
+
+    const handleClick = async () => {
+        const images = await Promise.all(files.map(file => file.arrayBuffer()));
+        const byteArrays = images.map(buffer => new Uint8Array(buffer));
+        test(byteArrays);
+    };
+
+    return (
+        <React.StrictMode>
+            <h1 className={"text-2xl text-black font-black"}>HEIC to JPEG Converter</h1>
+            <div className="mb-3 w-96">
+                <label
+                    htmlFor="formFileMultiple"
+                    className="mb-2 inline-block"
+                >
+                    Select Files to Convert
+                </label>
+                <input
+                    className="pointer relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+                    type="file"
+                    accept={".heic"}
+                    id="formFileMultiple"
+                    multiple
+                    onChange={handleFileChange}
+                />
+                <button className={"p-2 bg-blue-400 text-white rounded border hover:bg-blue-800"} onClick={handleClick}>Process Images</button>
+            </div>
+        </React.StrictMode>
+    );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Idx />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+async function test(images: Uint8Array[]) {
+    const wasmLocation = new URL('@imagemagick/magick-wasm/magick.wasm', import.meta.url).href;
+
+    const wasmResponse = await fetch(wasmLocation);
+    const wasmBytes = await wasmResponse.arrayBuffer();
+
+    await initializeImageMagick(wasmBytes);
+
+    images.forEach((image, index) => {
+        ImageMagick.read(image, (magickImage) => {
+            magickImage.write(MagickFormat.Jpeg, (jpegData) => {
+                const blob = new Blob([jpegData], { type: 'image/jpeg' });
+                const url = URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = `image_${index + 1}.jpeg`;
+                a.click();
+                URL.revokeObjectURL(url);
+            });
+        });
+    });
+}
